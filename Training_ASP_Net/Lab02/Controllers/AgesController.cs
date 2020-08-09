9232be5354d1257368d32e997cdad94baee81bd4
Thach Nguyen <kainguyen.vt@gmail.com>
2020-08-09 09:56:41 +0700

    [Feature] Create Ages and Category

---
+++ Training_ASP_Net/Lab02/Controllers/AgesController.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Lab02.Models;
+
+namespace Lab02.Controllers
+{
+    public class AgesController : Controller
+    {
+        private BabyShopEntities db = new BabyShopEntities();
+
+        // GET: Ages
+        public ActionResult Index()
+        {
+            return View(db.Ages.Where(a => a.IsDelete == false).ToList());
+        }
+
+        // GET: Ages/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Age age = db.Ages.Find(id);
+            if (age == null)
+            {
+                return HttpNotFound();
+            }
+            return View(age);
+        }
+
+        // GET: Ages/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Ages/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Name,IsActive,IsDelete,CreatedAt,UpdatedAt")] Age age)
+        {
+            if (ModelState.IsValid)
+            {
+                age.CreatedAt = DateTime.Now;
+                age.UpdatedAt = DateTime.Now;
+                db.Ages.Add(age);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(age);
+        }
+
+        // GET: Ages/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Age age = db.Ages.Find(id);
+            if (age == null)
+            {
+                return HttpNotFound();
+            }
+            return View(age);
+        }
+
+        // POST: Ages/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Name,IsActive,IsDelete,CreatedAt,UpdatedAt")] Age age)
+        {
+            if (ModelState.IsValid)
+            {
+                age.UpdatedAt = DateTime.Now;
+                db.Entry(age).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(age);
+        }
+
+        // GET: Ages/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Age age = db.Ages.Find(id);
+            if (age == null)
+            {
+                return HttpNotFound();
+            }
+            return View(age);
+        }
+
+        // POST: Ages/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Age age = db.Ages.Find(id);
+            db.Ages.Remove(age);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Training_ASP_Net/Lab02/Controllers/CategoriesController.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Security.Cryptography;
+using System.Web;
+using System.Web.Mvc;
+using Lab02.Models;
+
+namespace Lab02.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        private BabyShopEntities db = new BabyShopEntities();
+
+        // GET: Categories
+        public ActionResult Index()
+        {
+            return View(db.Categories.Where(c=>c.IsDelete == false).ToList());
+        }
+
+        // GET: Categories/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Category category = db.Categories.Find(id);
+            if (category == null)
+            {
+                return HttpNotFound();
+            }
+            return View(category);
+        }
+
+        // GET: Categories/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Categories/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Name,IsActive,IsDelete")] Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                category.CreatedAt = DateTime.Now;
+                category.UpdatedAt = DateTime.Now;
+                db.Categories.Add(category);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(category);
+        }
+
+        // GET: Categories/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Category category = db.Categories.Find(id);
+            if (category == null)
+            {
+                return HttpNotFound();
+            }
+            return View(category);
+        }
+
+        // POST: Categories/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Name,IsActive,IsDelete,CreatedAt,UpdatedAt")] Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                category.UpdatedAt = DateTime.Now;
+                db.Entry(category).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(category);
+        }
+
+        // GET: Categories/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Category category = db.Categories.Find(id);
+            if (category == null)
+            {
+                return HttpNotFound();
+            }
+            return View(category);
+        }
+
+        // POST: Categories/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Category category = db.Categories.Find(id);
+            db.Categories.Remove(category);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
